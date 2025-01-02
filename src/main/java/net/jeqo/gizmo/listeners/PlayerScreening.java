94bd9322841cf773c0bf9c1993d2b8dca2da75a9
IanTapply22 <iantapply22@gmail.com>
2025-01-01 19:01:23 -0500

    feat: Update to version 1.21.3

--- src/main/java/net/jeqo/gizmo/listeners/PlayerScreening.java
+++ src/main/java/net/jeqo/gizmo/listeners/PlayerScreening.java
@@ -131,7 +131,6 @@ public class PlayerScreening implements Listener {
                                     meta.addItemFlags(ItemFlag.HIDE_DESTROYS);
                                     meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
                                     meta.addItemFlags(ItemFlag.HIDE_PLACED_ON);
-                                    meta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
                                     meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
                                 }
                                 meta.setCustomModelData(keySection.getInt("custom-model-data"));
@@ -191,7 +190,6 @@ public class PlayerScreening implements Listener {
                                     meta.addItemFlags(ItemFlag.HIDE_DESTROYS);
                                     meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
                                     meta.addItemFlags(ItemFlag.HIDE_PLACED_ON);
-                                    meta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
                                     meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
                                 }
                                 meta.setCustomModelData(keySection.getInt("custom-model-data"));
